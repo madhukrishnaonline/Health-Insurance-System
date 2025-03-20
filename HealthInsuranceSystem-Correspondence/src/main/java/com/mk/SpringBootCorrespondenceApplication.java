755f91638306538d1f79f131a9bfe09b7c7d1eef
madhukrishnaonline <mk9032605@gmail.com>
2025-03-20 09:27:57 +0530

    latest commit

---
+++ HealthInsuranceSystem-Correspondence/src/main/java/com/mk/SpringBootCorrespondenceApplication.java
@@ -0,0 +1,12 @@
+package com.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCorrespondenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCorrespondenceApplication.class, args);
+	}//main
+}//class
---
+++ HealthInsuranceSystem-Correspondence/src/main/java/com/mk/entity/CitizenAppEntity.java
@@ -0,0 +1,57 @@
+package com.mk.entity;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "CITIZEN_APP")
+public class CitizenAppEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer app_id;
+	
+	@Column(length = 25)
+	private String fullname;
+	
+	@Column(length = 25)
+	private String email;
+	
+	private LocalDate dob;
+	
+	@Column(length = 10)
+	private Long phone;
+	
+	private Long ssn;
+	
+	@Column(length = 10)
+	private String gender;
+	
+	@Column(length = 15)
+	private String stateName;
+	
+	@CreationTimestamp
+	@Column(name = "CREATE_DATE")
+	private LocalDate createdate;
+	
+	@UpdateTimestamp
+	@Column(name = "UPDATE_DATE")
+	private LocalDate updatedate;
+	
+	@Column(length = 25)
+	private String createdBy;
+	
+	@Column(length = 25)
+	private String updatedBy;
+}//class
