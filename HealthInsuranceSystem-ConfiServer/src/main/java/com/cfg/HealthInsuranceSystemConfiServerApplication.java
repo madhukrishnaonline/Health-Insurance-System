1e02edd76a4f4ba58a19955d8d846bf5ddbe2e6d
madhukrishnaonline <mk9032605@gmail.com>
2025-03-20 09:27:31 +0530

    latest commit

---
+++ HealthInsuranceSystem-ConfiServer/src/main/java/com/cfg/HealthInsuranceSystemConfiServerApplication.java
@@ -0,0 +1,13 @@
+package com.cfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthInsuranceSystemConfiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthInsuranceSystemConfiServerApplication.class, args);
+	}
+
+}
---
+++ HealthInsuranceSystem-ConfiServer/src/test/java/com/cfg/HealthInsuranceSystemConfiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthInsuranceSystemConfiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
