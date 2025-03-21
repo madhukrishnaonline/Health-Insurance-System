959d38723b477b2d07ef3931f8556d7df7cdf005
madhukrishnaonline <mk9032605@gmail.com>
2025-03-21 22:48:11 +0530

    create Config-Server

--- HealthInsuranceSystem-ConfiServer/src/main/java/com/cfg/HealthInsuranceSystemConfiServerApplication.java
+++ HealthInsuranceSystem-ConfiServer/src/main/java/com/cfg/HealthInsuranceSystemConfiServerApplication.java
@@ -2,12 +2,13 @@ package com.cfg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HealthInsuranceSystemConfiServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthInsuranceSystemConfiServerApplication.class, args);
 	}
-
 }
