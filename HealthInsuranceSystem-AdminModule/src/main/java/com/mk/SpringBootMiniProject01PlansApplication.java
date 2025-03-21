490f5f664feef318bd912fd15a49d79ad2aca815
madhukrishnaonline <mk9032605@gmail.com>
2025-03-21 22:43:14 +0530

    update service

--- HealthInsuranceSystem-AdminModule/src/main/java/com/mk/SpringBootMiniProject01PlansApplication.java
+++ HealthInsuranceSystem-AdminModule/src/main/java/com/mk/SpringBootMiniProject01PlansApplication.java
@@ -2,21 +2,13 @@ package com.mk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication // (exclude = { DataSourceAutoConfiguration.class,
-						// HibernateJpaAutoConfiguration.class })
-//@EnableSwagger2
+@SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootMiniProject01PlansApplication {
-	
-	/*
-	 * @Bean Docket docket() { return new
-	 * Docket(DocumentationType.SWAGGER_2).select()
-	 * .apis(RequestHandlerSelectors.basePackage("com.mk.controller")).paths(
-	 * PathSelectors.any()).build(); }
-	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMiniProject01PlansApplication.class, args);
 	}
-
 }// class
