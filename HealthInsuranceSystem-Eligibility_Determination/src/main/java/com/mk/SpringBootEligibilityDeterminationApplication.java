330244a86f8886bfa45418af926b96488e7ecb12
madhukrishnaonline <mk9032605@gmail.com>
2025-03-21 22:50:12 +0530

    update service

--- HealthInsuranceSystem-Eligibility_Determination/src/main/java/com/mk/SpringBootEligibilityDeterminationApplication.java
+++ HealthInsuranceSystem-Eligibility_Determination/src/main/java/com/mk/SpringBootEligibilityDeterminationApplication.java
@@ -2,8 +2,10 @@ package com.mk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootEligibilityDeterminationApplication {
 
 	public static void main(String[] args) {
--- HealthInsuranceSystem-Eligibility_Determination/src/main/java/com/mk/entity/CoTriggerEntity.java
+++ HealthInsuranceSystem-Eligibility_Determination/src/main/java/com/mk/entity/CoTriggerEntity.java
@@ -1,24 +1,31 @@
 package com.mk.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "CO_TRIGGERs")
+@Table(name = "CO_TRIGGERS")
 public class CoTriggerEntity {
 
 	@Id
+	@Column(name="TRIGGER_ID")
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer triggerId;
 
+	@Column(name="CASE_NUM")
 	private Long caseNum;
 
+	@Lob
+	@Column(name="CO_PDF")
 	private byte[] coPdf;
 
+	@Column(name="TR_STATUS")
 	private String trStatus;
 }//class
